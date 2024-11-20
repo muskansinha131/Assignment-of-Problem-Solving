bad14d6bfc13fd61a73c73724268dcaf29cd1c6e
Muskan Sinha <muskansinha131@gmail.com>
2024-11-20 14:12:42 +0530

    display the size of every data type using sizeof operator

---
+++ a3.c
@@ -0,0 +1,12 @@
+// Write a program to display the size of every data type using “sizeof” operator. 
+
+#include<stdio.h>
+int main()
+{
+   printf("Size of char data type = %d \n",sizeof(char));
+   printf("Size of int data type = %d \n",sizeof(int));
+   printf("Size of float data type = %d\n",sizeof(float));
+   printf("Size of double data type = %d\n",sizeof(double));
+   printf("Size of long data type = %d\n",sizeof(long));
+    return 0 ;
+}
