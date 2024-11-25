d00480bfc2d7026b0eaa49bfb5311ac6fca4a103
Muskan Sinha <muskansinha131@gmail.com>
2024-11-26 00:29:30 +0530

    Swap two variables using pointers

---
+++ a37.c
@@ -0,0 +1,22 @@
+// Write a program to swap value of two variables using pointer. 
+
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Before swap: a = %d, b = %d\n", a, b);
+    swap(&a, &b);
+    printf("After swap: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
+
